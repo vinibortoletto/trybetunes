38bd3b1e022db01b3698881b14b1f5562e4ee228
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-08-30 16:37:48 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/" component={ Login } />
+          <Route component={ NotFound } />
+        </Switch>
+      </>
+    );
   }
 }
-
-// a
 
 export default App;
